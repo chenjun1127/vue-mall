e4c9110ce8a6d4b29346498088158d868287ee8d
Jone-Chen <402074940@qq.com>
2018-08-14 15:31:35 +0800

    :smile:

--- server/utils/mail.config.js
+++ server/utils/mail.config.js
@@ -2,10 +2,11 @@
  *  Created by jone-chen on 2018/1/16
  */
 module.exports = {
-    //邮件配置
+    // 邮件配置
+    // 邮箱密码保密，哈哈
     email: {
         service: 'QQ',
-        user: '402074940@qq.com',
-        pass: 'cj19861127.com',
+        //user: '...',
+        //pass: '...',
     }
 }
